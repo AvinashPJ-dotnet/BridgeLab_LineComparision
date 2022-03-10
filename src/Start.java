fa5ddd7e3627d53a3c32a105d8cc0c0992230570
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-03-10 12:33:11 +0530

    Added: length calculation method in LineComparision

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Start {
+    public static void main(String[] args) {
+        LineComparision obj = new LineComparision();
+        double length = obj.calculateLength(6, 7, 8, 8);
+        System.out.println("Length of Two point is: "+length);
+        }
+}
